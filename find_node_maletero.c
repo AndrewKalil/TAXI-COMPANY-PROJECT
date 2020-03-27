5e2d14f91d9b561dee26eb1eeab309394ea535af
AndrewKalil <1541@holbertonschool.com>
2020-03-27 17:00:35 -0500

    function to find first node with maletero

---
+++ find_node_maletero.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ *
+ */
+unsigned int find_node_maletero(list_t *head)
+{
+	unsigned int i;
+	list_t *index;
+
+	index = head;
+	if (head == NULL)
+		return (NULL);
+
+	for (i = 0; i < 73 && head; i++)
+	{
+		if (head == NULL)
+			return (NULL);
+		index = head->next;
+		head = position
+	}
+	return (position)
+}
